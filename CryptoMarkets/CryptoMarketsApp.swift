5486b818d30b6366f89b87271b2637e32cc5b7c9
stixlee <michael.lee.1983@iclllud.com>
2023-12-30 16:03:46 -0700

    Initial Commit

---
+++ CryptoMarkets/CryptoMarketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoMarketsApp.swift
+//  CryptoMarkets
+//
+//  Created by Michael Lee on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoMarketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
