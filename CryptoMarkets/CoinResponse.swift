cd3f78ae6ad2609df78d47a5ae983576e1864bbb
stixlee <michael.lee.1983@iclllud.com>
2024-01-16 08:21:06 -0700

    Adding more properties to CoinReponse

--- CryptoMarkets/CoinResponse.swift
+++ CryptoMarkets/CoinResponse.swift
@@ -130,6 +130,32 @@ struct MarketData: Codable {
     let volume: IntCollection
     let high24h: DoubleCollection
     let low24h: DoubleCollection
+    let priceChange24h: Double
+    let percentChange24h: Double
+    let percentChange7d: Double
+    let percentChange14d: Double
+    let percentChange30d: Double
+    let percentChange60d: Double
+    let percentChange200d: Double
+    let percentChange1y: Double
+    let marketCapChange24h: Double
+    let percentChangeMarketCap24h: Double
+    let priceChangeInCurrency24h: DoubleCollection
+    let percentChangeInCurrency1h: DoubleCollection
+    let percentChangeInCurrency24h: DoubleCollection
+    let percentChangeInCurrency7d: DoubleCollection
+    let percentChangeInCurrency14d: DoubleCollection
+    let percentChangeInCurrency30d: DoubleCollection
+    let percentChangeInCurrency60d: DoubleCollection
+    let percentChangeInCurrency200d: DoubleCollection
+    let percentChangeInCurrency1y: DoubleCollection
+    let marketCapChaningInCurrency24h: DoubleCollection
+    let percentChangeMarketCapInCurrency24h: DoubleCollection
+    let totalSupply: Int
+    let maxSupply: Int
+    let circulatingSupply: Int
+    let lastUpdated: String
+
 
     enum CodingKeys: String, CodingKey {
         case price = "current_price"
@@ -149,8 +175,32 @@ struct MarketData: Codable {
         case volume = "total_volume"
         case high24h = "high_24h"
         case low24h = "low_24h"
-
-    }
+        case priceChange24h = "price_change_24h"
+        case percentChange24h = "price_change_percentage_24h"
+        case percentChange7d = "price_change_percentage_7d"
+        case percentChange14d = "price_change_percentage_14d"
+        case percentChange30d = "price_change_percentage_30d"
+        case percentChange60d = "price_change_percentage_60d"
+        case percentChange200d = "price_change_percentage_200d"
+        case percentChange1y = "price_change_percentage_1y"
+        case marketCapChange24h = "market_cap_change_24h"
+        case percentChangeMarketCap24h = "market_cap_change_percentage_24h"
+        case priceChangeInCurrency24h = "price_change_24h_in_currency"
+        case percentChangeInCurrency1h = "price_change_percentage_1h_in_currency"
+        case percentChangeInCurrency24h = "price_change_percentage_24h_in_currency"
+        case percentChangeInCurrency7d = "price_change_percentage_7d_in_currency"
+        case percentChangeInCurrency14d = "price_change_percentage_14d_in_currency"
+        case percentChangeInCurrency30d = "price_change_percentage_30d_in_currency"
+        case percentChangeInCurrency60d = "price_change_percentage_60d_in_currency"
+        case percentChangeInCurrency200d = "price_change_percentage_200d_in_currency"
+        case percentChangeInCurrency1y = "price_change_percentage_1y_in_currency"
+        case marketCapChaningInCurrency24h = "market_cap_change_24h_in_currency"
+        case percentChangeMarketCapInCurrency24h = "market_cap_change_percentage_24h_in_currency"
+        case totalSupply = "total_supply"
+        case maxSupply = "max_supply"
+        case circulatingSupply = "circulating_supply"
+        case lastUpdated = "last_updated"
+   }
 }
 
 
