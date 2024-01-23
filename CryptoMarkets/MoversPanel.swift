6fda024065c63b93741c792b05e5819ba2262f06
stixlee <michael.lee.1983@iclllud.com>
2024-01-23 13:50:04 -0700

    Movers Panel

---
+++ CryptoMarkets/MoversPanel.swift
@@ -0,0 +1,18 @@
+//
+//  MoversPanel.swift
+//  CryptoMarkets
+//
+//  Created by Michael Lee on 1/22/24.
+//
+
+import SwiftUI
+
+struct MoversPanel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoversPanel()
+}
