23961d5f72e5f51a4544171f3c68b2736cd947b6
stixlee <michael.lee.1983@iclllud.com>
2024-01-29 09:22:20 -0700

    Removed old code

--- CryptoMarkets/RightSideMenuView.swift
+++ CryptoMarkets/RightSideMenuView.swift
@@ -22,3 +22,10 @@ struct RightSideMenuView: View {
 #Preview {
     RightSideMenuView()
 }
+
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
+
